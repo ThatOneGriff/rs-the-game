a85f7aa4d6c76f2633d5d8aae963ffff4578b132
ThatOneGriff <temka.pervakov@gmail.com>
2025-12-03 14:56:35 +0300

    Initial commit

---
+++ project/main.c
@@ -0,0 +1,26 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_timer.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* = Library information =
+SDL version: 3.2.4 (last updated 10.11.2025)
+nlohmann/json version: TBA
+
+ = Compiler args (in order) =
+gcc main.c -o ../main.exe -I "C:/SDL/x86_64-w64-mingw32/include" -L "C:/SDL/x86_64-w64-mingw32/lib" -lSDL3 -I "C:/SDL_Image/x86_64-w64-mingw32/include" -L "C:/SDL_image/x86_64-w64-mingw32/lib" -lSDL3_image -I "C:/SDL_ttf/x86_64-w64-mingw32/include" -L "C:/SDL_ttf/x86_64-w64-mingw32/lib" -lSDL3_ttf -Ofast -Wall -Werror
+../main.exe
+*/
+
+
+
+#define SDL_MAIN_HANDLED
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
