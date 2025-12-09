fd25595ec8be60ad968cdb895420c1068b5ec7e7
ThatOneGriff <temka.pervakov@gmail.com>
2025-12-09 22:56:59 +0300

    Miniaudio added.

---
+++ project/audio/audio.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include "miniaudio.h"
+
+/*struct Audio
+{
+    ma_engine engine;
+    ma_sound  bg_music;
+};
+static struct Audio audio;*/
+
+#endif /// AUDIO_H
--- project/main.c
+++ project/main.c
@@ -4,6 +4,7 @@
 #include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+#include "audio/audio.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,11 +13,7 @@ SDL version: 3.2.28 (last updated 06.12.2025)
 nlohmann/json version: TBA
 
  = Compiler args (in order) =
-<<<<<<< HEAD
-gcc -Ofast -Wall -Werror project/main.c -o ./main.exe -I "C:/SDL/x86_64-w64-mingw32/include" -I "C:/SDL_Image/x86_64-w64-mingw32/include" -I "C:/SDL_ttf/x86_64-w64-mingw32/include" -L "C:/SDL/x86_64-w64-mingw32/lib" -L "C:/SDL_image/x86_64-w64-mingw32/lib" -L "C:/SDL_ttf/x86_64-w64-mingw32/lib" -lSDL3 -lSDL3_image -lSDL3_ttf
-=======
-gcc -Ofast -Wall -Werror -std=c17 project/main.c -o ./main.exe -I "C:/SDL/x86_64-w64-mingw32/include" -I "C:/SDL_Image/x86_64-w64-mingw32/include" -I "C:/SDL_ttf/x86_64-w64-mingw32/include" -L "./SDL3-x64" -lSDL3 -lSDL3_image -lSDL3_ttf -static
->>>>>>> 05a4058bcb0fcda70cc9d28c100f13badb5af9f1
+gcc -Ofast -Wall -Werror -std=c17 project/main.c project/audio/audio.c -o ./main.exe -I "C:/SDL/x86_64-w64-mingw32/include" -I "C:/SDL_Image/x86_64-w64-mingw32/include" -I "C:/SDL_ttf/x86_64-w64-mingw32/include" -L "C:/SDL/x86_64-w64-mingw32/lib" -L "C:/SDL_image/x86_64-w64-mingw32/lib" -L "C:/SDL_ttf/x86_64-w64-mingw32/lib" -lSDL3 -lSDL3_image -lSDL3_ttf
 ./main.exe
 */
 
