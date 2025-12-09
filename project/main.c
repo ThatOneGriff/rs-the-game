3cb86a2aa0ac034dc96441e20e2fd863465ae4f8
ThatOneGriff <temka.pervakov@gmail.com>
2025-12-09 22:16:31 +0300

    changes before static linking

--- project/main.c
+++ project/main.c
@@ -12,8 +12,8 @@ SDL version: 3.2.28 (last updated 06.12.2025)
 nlohmann/json version: TBA
 
  = Compiler args (in order) =
-gcc main.c -o ../main.exe -I "C:/SDL/x86_64-w64-mingw32/include" -L "C:/SDL/x86_64-w64-mingw32/lib" -lSDL3 -I "C:/SDL_Image/x86_64-w64-mingw32/include" -L "C:/SDL_image/x86_64-w64-mingw32/lib" -lSDL3_image -I "C:/SDL_ttf/x86_64-w64-mingw32/include" -L "C:/SDL_ttf/x86_64-w64-mingw32/lib" -lSDL3_ttf -Ofast -Wall -Werror
-../main.exe
+gcc project/main.c -o ./main.exe -I "C:/SDL/x86_64-w64-mingw32/include" -L "C:/SDL/x86_64-w64-mingw32/lib" -lSDL3 -I "C:/SDL_Image/x86_64-w64-mingw32/include" -L "C:/SDL_image/x86_64-w64-mingw32/lib" -lSDL3_image -I "C:/SDL_ttf/x86_64-w64-mingw32/include" -L "C:/SDL_ttf/x86_64-w64-mingw32/lib" -lSDL3_ttf -Ofast -Wall -Werror -std=c17
+./main.exe
 */
 
 /// NOTE: use texture scaling mode `SDL_SCALING_PIXELART`.
