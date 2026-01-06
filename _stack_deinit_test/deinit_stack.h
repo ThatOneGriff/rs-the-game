529c925a9c89cd6c9a32431f70e14f8ca86cccc3
ThatOneGriff <temka.pervakov@gmail.com>
2026-01-06 18:50:58 +0300

    ✅ Basic stack implementation

--- _stack_deinit_test/deinit_stack.h
+++ _stack_deinit_test/deinit_stack.h
@@ -2,11 +2,18 @@
 #ifndef DEINIT_STACK_H
 #define DEINIT_STACK_H
 
+//#include "debug.h" /// Error printing.
+#include <stdlib.h> /// `*alloc()`.
+
+/// TEMP: `void*` replaced with `int` for elements.
 
 /* Predef */
 
 struct Deinit_Stack;
-struct Deinit_Stack new_deinit_stack(const size_t size);
+struct Deinit_Stack new_deinit_stack(const size_t size, int* exit_code);
+void               free_deinit_stack(struct Deinit_Stack* target); 
+void             add_to_deinit_stack(struct Deinit_Stack* target, const int new_element);
+int            pop_from_deinit_stack(struct Deinit_Stack* target);
 
 
 /* Struct */
@@ -16,11 +23,82 @@ struct Deinit_Stack
     size_t max_elements;
     size_t cur_elements;
     int* elements;
+};
+
+struct Deinit_Stack new_deinit_stack(const size_t size, int* exit_code)
+{
+    if (exit_code == NULL)
+        {;} /// TEMP
+    //    print_warning("`new_deinit_stack()`: `exit_code` arg is `NULL`", NON_SDL_ERROR);
+    struct Deinit_Stack result;
+    result.cur_elements = 0;
+    result.max_elements = 0; /// To be reset once memory is successfully allocated.
+
+    result.elements = calloc(size, sizeof(int));
+    if (result.elements == NULL)
+    {
+        //print_error("`new_deinit_stack()`: couldn't allocate memory", NON_SDL_ERROR);
+        //*exit_code = EXIT_FAILURE;
+        return result;
+    }
+    result.max_elements = size;
+
+    *exit_code = EXIT_SUCCESS;
+    return result;
 }
 
-void add(struct Deinit_Stack* target)
+void free_deinit_stack(struct Deinit_Stack* target)
 {
+    if (target == NULL)
+        return;
+    
+    if (target->elements != NULL)
+    {
+        free(target->elements); /// NOTE: take double pointers into account if you happen to use them in the future.
+        target->elements = NULL;
+    }
+    target->max_elements = 0;
+    target->cur_elements = 0;
+    return;
+}
 
+
+/* Body */
+
+/// NOTE: unlike other structures (`Multi/Shifting_Texture`), which have a use case to being dynamic,
+/// making `Deinit_Stack` non-fixed-size could cause confusion and unneccessary memory usage
+/// without good benefits. So, the size of `Deinit_Stack::elements` is set once, upon creation.
+void add_to_deinit_stack(struct Deinit_Stack* target, const int new_element)
+{
+    if (target == NULL || target->elements == NULL)
+    {
+        //print_error("`add_to_deinit_stack()`: stack or its `elements` are `NULL`", NON_SDL_ERROR);
+        return;
+    }
+    if (target->cur_elements == target->max_elements)
+    {
+        //print_error("`add_to_deinit_stack()`: stack is full", NON_SDL_ERROR);
+        return;
+    }
+
+    target->elements[target->cur_elements++] = new_element;
+}
+
+
+int pop_from_deinit_stack(struct Deinit_Stack* target)
+{
+    if (target == NULL || target->elements == NULL)
+    {
+        //print_error("`pop_from_deinit_stack()`: stack or its `elements` are `NULL`", NON_SDL_ERROR);
+        return 0;
+    }
+    if (target->cur_elements == 0)
+    {
+        //print_error("`pop_from_deinit_stack()`: stack is empty", NON_SDL_ERROR);
+        return 0;
+    }
+
+    return target->elements[--target->cur_elements];
 }
 
 #endif /// DEINIT_STACK_H
--- _stack_deinit_test/test.c
+++ _stack_deinit_test/test.c
@@ -3,13 +3,32 @@
 #include "deinit_stack.h"
 
 /*
-gcc _stack_deinit_test/test.c -o _stack_deinit_test/test.exe -Wall -Werror -Wextra -std=c17
-./_stack_deinit_test/test.exe
+gcc test.c -o test.exe -Wall -Werror -Wextra -std=c23
+./test.exe
 */
 
 
 int main()
 {
+    int exit_code = EXIT_SUCCESS;
+    struct Deinit_Stack test_stack = new_deinit_stack(5, &exit_code);
+    if (exit_code == EXIT_FAILURE)
+    {
+        //error message print if needed
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    char inp = '\0';
+    for (int i = 0; i < 5; i++)
+    {
+        inp = getchar();
+        add_to_deinit_stack(&test_stack, (int)(inp - '0'));
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", pop_from_deinit_stack(&test_stack));
+    }
+
+    return exit_code;
 }
